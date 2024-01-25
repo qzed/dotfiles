e2bfbedde63f68107f3ea41ae359e2e270b2c2e4
Maximilian Luz <m@mxnluz.io>
2024-01-25 18:59:41 +0100

    Fix eza alias definitions

--- home/dot_config/zsh/directories.zsh
+++ home/dot_config/zsh/directories.zsh
@@ -11,10 +11,10 @@ alias pd="popd"
 
 # Sortcuts for `ls`
 if is_command "eza" ; then          # Use `eza` if available
-    alias ll="exa -lhb --group-directories-first --sort=Name"
-    alias la="exa -lhba --group-directories-first --sort=Name"
-    alias ls="exa --group-directories-first --sort=Name"
-    alias t="exa -T --group-directories-first --sort=Name"
+    alias ll="eza -lhb --group-directories-first --sort=Name"
+    alias la="eza -lhba --group-directories-first --sort=Name"
+    alias ls="eza --group-directories-first --sort=Name"
+    alias t="eza -T --group-directories-first --sort=Name"
 elif is_command "exa" ; then          # Use `exa` if available (deprecated, prefer eza)
     alias ll="exa -lhb --group-directories-first --sort=Name"
     alias la="exa -lhba --group-directories-first --sort=Name"
